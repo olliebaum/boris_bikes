b27cc4b089e03dd2c2a976ef971c7d8b5fe3eef0
olliebaum <oliver@baums.org.uk>
2018-09-05 17:58:47 +0100

    Completed steps 10 + 11

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,4 +1,14 @@
 class DockingStation
+  attr_reader :bikes
+  def initialize
+    @bikes = []
+  end
   def release_bike
-  end 
+    Bike.new
+  end
+
+  def dock(bike)
+    @bikes << bike
+  end
+
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,4 +1,20 @@
 require 'DockingStation'
 describe DockingStation do
   it { should respond_to(:release_bike) }
+  it 'release_bike returns a Bike' do
+    expect(subject.release_bike).to be_an_instance_of(Bike)
+  end
+  it "expects the bike to be working" do
+    expect(subject.release_bike.working?).to eq true
+  end
+  it { should respond_to(:dock) }
+  it "has a variable called bikes that is an array" do
+    expect(subject.bikes).to be_an_instance_of(Array)
+  end
+  it "After using the dock method, stores the bike in the bikes variable" do
+    bike_to_dock = Bike.new
+    subject.dock(bike_to_dock)
+    expect(subject.bikes.include? bike_to_dock).to eq true
+
+  end
 end
